235a43539fa2bd999b5b48b219397f227194fee7
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2023-02-03 22:28:30 +0100

    update

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@ output "custom_domain_verification_id" {
   value = azurerm_app_service.this.custom_domain_verification_id
 }
 
-output "default_hostname" {
-  value = azurerm_app_service.this.default_hostname
+output "default_site_hostname" {
+  value = azurerm_app_service.this.default_site_hostname
 }
