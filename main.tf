c4ade5211bbca9020a36adf07ce036cf220c5b82
Moosavinezhad <pmoosavinezhad@alegri.eu>
2023-06-13 13:11:05 +0200

    update

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ resource "azurerm_app_service" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = var.app_service_plan_id
+  site_config {
+    always_on                = var.site_config.always_on
+    dotnet_framework_version = var.site_config.dotnet_framework_version
+    ftps_state               = var.site_config.ftps_state
+    cors {
+      allowed_origins = var.site_config.cors.allowed_origins
+    }
+  }
   tags = merge(
     var.additional_tags,
     {
